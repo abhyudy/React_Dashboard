c0a8b03b6ecc2cebe5d6c12f0c2775c24861fe0c
Abhyudya Singh Ujala <abhyudyasinghujala2002@gmail.com>
2024-08-19 22:02:20 +0530

    ******

--- src/components/CWPP/CWPP.js
+++ src/components/CWPP/CWPP.js
@@ -3,37 +3,34 @@ import "./CWPP.css"; // Import specific styles for CWPP
 
 const CWPP = () => {
   return (
-    <div className="cwpp-dashboard">
-      <div className="row">
-        <div className="col-md-4">
-          <div className="cwpp-title">Top 5 Namespace Specific Alerts</div>
-          <div className="cwpp-content">
-            <p>No graph data available</p>
-            <img
-              src="path-to-graph-font-image.png"
-              alt="Graph Placeholder"
-              className="graph-placeholder"
-            />
-          </div>
-        </div>
+    <>
+      <div className="nametitle">
+        <h2>CWPP Dashboard</h2>
+        <div className="cwpp-dashboard">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="cwpp-title">Top 5 Namespace Specific Alerts</div>
+              <div className="cwpp-content">
+                <i className="fa-sharp fa-solid fa-chart-mixed graph-placeholder"></i>
+                <p>No graph data available</p>
+              </div>
+            </div>
 
-        <div className="col-md-4">
-          <div className="cwpp-title">Workload Alerts</div>
-          <div className="cwpp-content">
-            <p>No graph data available</p>
-            <img
-              src="path-to-graph-font-image.png"
-              alt="Graph Placeholder"
-              className="graph-placeholder"
-            />
+            <div className="col-md-4">
+              <div className="cwpp-title">Workload Alerts</div>
+              <div className="cwpp-content">
+                <i className="fa-sharp fa-solid fa-chart-mixed graph-placeholder"></i>
+                <p>No graph data available</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="cwpp-add-widget">
+            <button className="add-widget-button">Add Widget</button>
           </div>
         </div>
       </div>
-
-      <div className="cwpp-add-widget">
-        <button className="add-widget-button">Add Widget</button>
-      </div>
-    </div>
+    </>
   );
 };
 
