64bc64e672182d1081b15ea3f3b0990b7b88de1f
Abhyudya Singh Ujala <abhyudyasinghujala2002@gmail.com>
2024-08-24 23:12:38 +0530

    done

--- src/components/CWPP/CWPP.js
+++ src/components/CWPP/CWPP.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChartBar } from "@fortawesome/free-solid-svg-icons";
 import "./CWPP.css";
 
 const CWPP = () => {
@@ -8,6 +10,9 @@ const CWPP = () => {
       <div className="dashboard-row">
         <div className="dashboard-card">
           <h3>Top 5 Namespace Specific Alerts</h3>
+          <div className="graph">
+            <FontAwesomeIcon icon={faChartBar} />
+          </div>
           <div className="dashboard-card-content">
             <p>No Graph data available</p>
           </div>
@@ -15,10 +20,14 @@ const CWPP = () => {
 
         <div className="dashboard-card">
           <h3>Workload Alerts</h3>
+          <div className="graph">
+            <FontAwesomeIcon icon={faChartBar} />
+          </div>
           <div className="dashboard-card-content">
             <p>No Graph data available</p>
           </div>
         </div>
+
         <div className="dashboard-card add-widget-card">
           <div className="dashboard-card-content">
             <button className="add-widget-button">+ Add Widget</button>
